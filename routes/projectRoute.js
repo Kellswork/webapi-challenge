1e66157a6f408938d193a4cafbc299bce493c0aa
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-12 09:44:05 +0100

    implement delete and put api endpoint for projects

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -24,13 +24,36 @@ router.get('/:id', validateProjectId, async (req, res) => {
 
 router.post('/', validateProjectData, async (req, res) => {
   try {
-    const { name, description, completed } = req.body;
-    const project = await db.insert({ name, description, completed: false });
+    const project = await db.insert(req.body);
     res.status(200).json(project);
   } catch (error) {
     res
       .status(500)
       .json({ error: 'Project data could not be saved to the database' });
+  }
+});
+
+router.put('/:id', validateProjectId, validateProjectData, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let upDatedProject = await db.update(id, req.body);
+    upDatedProject = await db.get(id);
+    res.status(200).json(upDatedProject);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'Project data could not be saved to the database' });
+  }
+});
+
+router.delete('/:id', validateProjectId, async (req, res) => {
+  try {
+    const { id } = req.params;
+    let deletedProject = await db.remove(id);
+    deletedProject = await db.get();
+    res.status(200).json(deletedProject);
+  } catch (error) {
+    res.status(500).json({ error: 'Project with ID could not be retrieved' });
   }
 });
 
@@ -53,14 +76,12 @@ async function validateProjectId(req, res, next) {
 async function validateProjectData(req, res, next) {
   try {
     const { name, description } = req.body;
-    if (!Object.keys(req.body)) {
+    if (!name || !description) {
       return res.status(400).json({ message: 'missing project data' });
     } else if (name === '' || name.length < 3) {
-      return res
-        .status(400)
-        .json({
-          error: 'name is required and cannot be less than 3 characters'
-        });
+      return res.status(400).json({
+        error: 'name is required and cannot be less than 3 characters'
+      });
     } else if (description === '' || description.length < 5) {
       return res.status(400).json({
         error: 'description is required and cannot be less than 5 characters'
